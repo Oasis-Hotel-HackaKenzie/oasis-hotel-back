1ca2be9f2289a703635b886b4eeacb2fbd6dfda3
leonardo-cb <leo.cb@hotmail.co.uk>
2023-11-27 11:51:43 -0300

    develop: room and occupation routes

--- src/controllers/rooms.controller.ts
+++ src/controllers/rooms.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { createRoomService, readAllRoomsService, readRoomService } from "../services/rooms.service";
+import { createOccupationService, createRoomService, deleteOccupationService, readAllRoomsService, readOccupationByCpfService, readRoomService } from "../services/rooms.service";
 
 export const createRoomController = async (req: Request, res: Response): Promise<Response> => {
     const room = await createRoomService(req.body)
@@ -14,7 +14,25 @@ export const readAllRoomsController = async (req: Request, res: Response): Promi
 }
 
 export const readRoomController = async (req: Request, res: Response): Promise<Response> => {
-    const room = await readRoomService(req.params.id)
+    const room = await readRoomService(req.params.room_id)
     
     return res.status(200).json(room)
+}
+
+export const createOccupationController = async (req: Request, res: Response): Promise<Response> => {
+    const occupation = await createOccupationService(req.body)
+
+    return res.status(201).json(occupation)
+}
+
+export const readOccupationByCpfController = async (req: Request, res: Response): Promise<Response> => {
+    const occupation = await readOccupationByCpfService(req.params.cpf)
+    
+    return res.status(200).json(occupation)
+}
+
+export const deleteOccupationController = async (req: Request, res: Response): Promise<Response> => {
+    await deleteOccupationService(req.params.occupation_id)
+    
+    return res.status(204).json()
 }
--- src/routers/room.routes.ts
+++ src/routers/room.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express"
-import { createRoomController, readAllRoomsController, readRoomController } from "../controllers/rooms.controller"
+import { createOccupationController, createRoomController, deleteOccupationController, readAllRoomsController, readOccupationByCpfController, readRoomController } from "../controllers/rooms.controller"
 
 export const roomRouter: Router = Router()
 
 roomRouter.post("/", createRoomController)
 roomRouter.get("/", readAllRoomsController)
 
-roomRouter.get("/:id", readRoomController)
+roomRouter.get("/:room_id", readRoomController)
+
+roomRouter.post("/occupation", createOccupationController)
+roomRouter.get("/occupation/:cpf", readOccupationByCpfController)
+roomRouter.delete("/occupation/:occupation_id", deleteOccupationController)
